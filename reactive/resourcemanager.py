f74e991f171bd04d89506d1c8ff333dbafcefd9d
Cory Johns <johnsca@gmail.com>
2016-01-29 11:58:30 -0500

    Fix mistake in slaves list key

--- reactive/resourcemanager.py
+++ reactive/resourcemanager.py
@@ -129,7 +129,7 @@ def unregister_nodemanager(nodemanager):
     yarn = YARN(hadoop)
     nodes_leaving = nodemanager.nodes()  # only returns nodes in "leaving" state
 
-    slaves = unitdata.kv().get('nodemanager.slaves', [])
+    slaves = unitdata.kv().get('resourcemanager.slaves', [])
     slaves_leaving = [node['host'] for node in nodes_leaving]
     hookenv.log('Slaves leaving: {}'.format(slaves_leaving))
 
