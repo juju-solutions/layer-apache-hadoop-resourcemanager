e5ff44e255aa4484100553a6d86a9c0bb39ffa26
Cory Johns <johnsca@gmail.com>
2016-03-14 14:26:24 -0400

    Fix test

--- tests/01-basic-deployment.py
+++ tests/01-basic-deployment.py
@@ -6,7 +6,7 @@ import amulet
 
 class TestDeploy(unittest.TestCase):
     """
-    Trivial deployment test for Apache Hadoop YARN Master.
+    Trivial deployment test for Apache Hadoop ResourceManager.
 
     This charm cannot do anything useful by itself, so integration testing
     is done in the bundle.
@@ -14,10 +14,10 @@ class TestDeploy(unittest.TestCase):
 
     def test_deploy(self):
         self.d = amulet.Deployment(series='trusty')
-        self.d.add('yarn-master', 'apache-hadoop-yarn-master')
+        self.d.add('resourcemanager', 'apache-hadoop-resourcemanager')
         self.d.setup(timeout=900)
         self.d.sentry.wait(timeout=1800)
-        self.unit = self.d.sentry['yarn-master'][0]
+        self.unit = self.d.sentry['resourcemanager'][0]
 
 
 if __name__ == '__main__':
